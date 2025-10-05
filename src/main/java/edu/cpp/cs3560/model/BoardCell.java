fd42aededc15f21049ce741b21c1b466530db7e7
Grace Hudgens <54286731+Grace0Hud@users.noreply.github.com>
2025-10-05 12:12:58 -0700

    board cell class.

---
+++ src/main/java/edu/cpp/cs3560/model/BoardCell.java
@@ -0,0 +1,131 @@
+package edu.cpp.cs3560.model;
+
+public class BoardCell
+{
+    final int level;
+    private Card[] cards;
+    private boolean selected;
+    private boolean allMatched;
+
+    public BoardCell(int level)
+    {
+	  this.level = level;
+	  cards = new Card[level];
+	  initializeCards();
+    }
+
+    private void initializeCards()
+    {
+	  for(int i = 0; i < level; i++)
+	  {
+		cards[i] = new Card(0);
+	  }
+    }
+    public int getLevel()
+    {
+	  return level;
+    }
+
+    public int getIdAt(int level)
+    {
+	  return cards[level].getId();
+    }
+
+    public void setCardId(int level, int id)
+    {
+	  cards[level].setId(id);
+    }
+    public void setCardAt(int level, Card card)
+    {
+	  cards[level] = card;
+    }
+    public Card[] getCards()
+    {
+	  return cards;
+    }
+    public Card getCardAt(int level)
+    {
+	  return cards[level];
+    }
+    public void selectCell()
+    {
+	  this.selected = true;
+    }
+
+    public void deSelectCell()
+    {
+	  this.selected = false;
+    }
+
+    public boolean isSelected()
+    {
+	  return selected;
+    }
+
+    public boolean matchCell(BoardCell other)
+    {
+	  if (other == null || other.level != level)
+	  {
+		return false;
+	  }
+	  Card[] otherCards = other.getCards();
+	  for(int i = 0; i < other.getLevel(); i++)
+	  {
+		if (this.matchCard(otherCards[i].getId()))
+		{
+		    other.markCardMatched(i);
+		    return true;
+		}
+	  }
+	  return false;
+    }
+    public boolean matchCard(int id)
+    {
+	  for(Card card : cards)
+	  {
+		if(card.getId() == id && !card.isMatched())
+		{
+		    card.markMatched();
+		    return true;
+		}
+	  }
+	  return false;
+    }
+    public boolean isCardMatched(int level)
+    {
+	  return cards[level].isMatched();
+    }
+
+    public void markCardMatched(int level)
+    {
+	  cards[level].markMatched();
+    }
+
+    public boolean isAllMatched()
+    {
+	  checkAllMatched();
+	  return allMatched;
+    }
+
+    private void checkAllMatched()
+    {
+	  for (Card card : cards)
+	  {
+		if (!card.isMatched())
+		{
+		    this.allMatched = false;
+		}
+	  }
+	  this.allMatched = true;
+    }
+
+    public String toString()
+    {
+	  StringBuilder str = new StringBuilder();
+	  for(Card card : cards)
+	  {
+		str.append(card.toString() + "\n");
+	  }
+	  return str.toString();
+    }
+}
---
+++ src/test/java/edu/cpp/cs3560/model/BoardCellTest.java
@@ -0,0 +1,109 @@
+package edu.cpp.cs3560.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardCellTest
+{
+    BoardCell cell;
+
+    @BeforeEach
+    void setUp()
+    {
+	  cell = new BoardCell(2);
+    }
+
+    @Test
+    void getLevel()
+    {
+	  assertEquals(2,cell.getLevel());
+    }
+
+    @Test
+    void setCardId()
+    {
+	  cell.setCardId(0, 3);
+	  assertEquals(3, cell.getIdAt(0));
+    }
+
+    @Test
+    void setCardAt()
+    {
+        Card card = new Card(3);
+        cell.setCardAt(0,card);
+        assertEquals(card,cell.getCardAt(0));
+    }
+
+    @Test
+    void getCards()
+    {
+        Card[] cards = cell.getCards();
+        assertEquals(2,cards.length);
+    }
+
+    @Test
+    void getCardAt()
+    {
+        Card card = new Card(3);
+        cell.setCardAt(0, card);
+        assertEquals(card,cell.getCardAt(0));
+    }
+
+    @Test
+    void selectCell()
+    {
+        assertFalse(cell.isSelected());
+        cell.selectCell();
+        assertTrue(cell.isSelected());
+    }
+
+    @Test
+    void deSelectCell()
+    {
+        cell.selectCell();
+        assertTrue(cell.isSelected());
+        cell.deSelectCell();
+        assertFalse(cell.isSelected());
+    }
+
+    @Test
+    void matchCell()
+    {
+        BoardCell cell2 = new BoardCell(2);
+        assertTrue(cell.matchCell(cell2));
+        assertTrue(cell.getCardAt(0).isMatched());
+        assertTrue(cell2.getCardAt(0).isMatched());
+    }
+
+    @Test
+    void matchCard()
+    {
+        assertTrue(cell.matchCard(0));
+        assertTrue(cell.isCardMatched(0));
+        assertFalse(cell.isCardMatched(1));
+    }
+
+    @Test
+    void markCardMatched()
+    {
+        cell.markCardMatched(0);
+        assertTrue(cell.isCardMatched(0));
+    }
+
+    @Test
+    void isAllMatched()
+    {
+        cell.markCardMatched(0);
+        cell.markCardMatched(1);
+        assertTrue(cell.isAllMatched());
+    }
+
+    @Test
+    void testToString()
+    {
+    }
+}
